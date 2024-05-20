e5760429110afd73475d253309b7e65264836137
aika_pdnw <apocalipsemercenary@gmail.com>
2024-05-20 20:47:55 +0300

    Passrat

--- modules/calmodule.py
+++ modules/calmodule.py
@@ -2,6 +2,6 @@ def sleepcal(m, rows, e, i):
     m[i].set(m[i].get() + 1)
     
     for c in range(rows):     
-        
+        pass
         if c>0: e[c].set(e[c-1].get() + m[c].get())
         else: e[0].set(m[0].get())
