6f7fcda46f0b2f814ffd1f8f4ce2d259c32d0577
aika_pdnw <apocalipsemercenary@gmail.com>
2025-02-25 22:41:12 +0300

    Small refactor

--- modules/actionWindowUI.py
+++ modules/actionWindowUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter import IntVar,StringVar
 from modules.gameState import tdelta, rows
 from modules.actions import SleepAction
@@ -26,7 +27,7 @@ def openActionWindow(bt, i, rows,):
     ActionWindow.title("Action")
 
     # sets the geometry of toplevel
-    ActionWindow.geometry("800x400")
+    ActionWindow.geometry("800x500")
     for c in range(rows):
         bt[c].configure(state=["disabled"])
 
@@ -40,29 +41,58 @@ def openActionWindow(bt, i, rows,):
     sleepScale = Scale(ActionWindow, orient=HORIZONTAL, length=200, from_=1.0, to=8.0, variable = sdelta)
     sleepScale.grid(row=1, column=0, sticky=W, pady=2)
 
-    cleanLb = Label(ActionWindow, text="Sleep")
+    cleanLb = Label(ActionWindow, text="Clean")
     cleanLb.grid(row=3, column=0, sticky=W, pady=2)
 
-    n = StringVar()
-    cleanCombobox = Combobox(ActionWindow, textvariable=n)
+    n1 = StringVar()
+    cleanCombobox = ttk.Combobox(ActionWindow, textvariable=n1)
     cleanCombobox['values'] = ('a','b')
     cleanCombobox.grid(row=4, column=0, sticky=W, pady=2)
-    cleanCombobox.current(1)
 
-    cookLb = Label(ActionWindow, text="Sleep")
+    cookLb = Label(ActionWindow, text="Cook")
     cookLb.grid(row=6, column=0, sticky=W, pady=2)
 
-    workLb = Label(ActionWindow, text="Sleep")
+    n2 = StringVar()
+    cookCombobox = ttk.Combobox(ActionWindow, textvariable=n2)
+    cookCombobox['values'] = ('a', 'b')
+    cookCombobox.grid(row=7, column=0, sticky=W, pady=2)
+
+    workLb = Label(ActionWindow, text="Work")
     workLb.grid(row=9, column=0, sticky=W, pady=2)
 
-    studyLb = Label(ActionWindow, text="Sleep")
+    n3 = StringVar()
+    workCombobox = ttk.Combobox(ActionWindow, textvariable=n3)
+    workCombobox['values'] = ('a', 'b')
+    workCombobox.grid(row=10, column=0, sticky=W, pady=2)
+
+    studyLb = Label(ActionWindow, text="Study")
     studyLb.grid(row=12, column=0, sticky=W, pady=2)
 
-    gymLb = Label(ActionWindow, text="Sleep")
+    n4 = StringVar()
+    studyCombobox = ttk.Combobox(ActionWindow, textvariable=n4)
+    studyCombobox['values'] = ('a', 'b')
+    studyCombobox.grid(row=13, column=0, sticky=W, pady=2)
+
+    gymLb = Label(ActionWindow, text="Gym")
     gymLb.grid(row=15, column=0, sticky=W, pady=2)
 
-    socialLb = Label(ActionWindow, text="Sleep")
+    n5 = StringVar()
+    gymCombobox = ttk.Combobox(ActionWindow, textvariable=n5)
+    gymCombobox['values'] = ('a', 'b')
+    gymCombobox.grid(row=16, column=0, sticky=W, pady=2)
+
+    socialLb = Label(ActionWindow, text="Social")
     socialLb.grid(row=18, column=0, sticky=W, pady=2)
 
-    hobbyLb = Label(ActionWindow, text="Sleep")
+    n6 = StringVar()
+    socialCombobox = ttk.Combobox(ActionWindow, textvariable=n6)
+    socialCombobox['values'] = ('a', 'b')
+    socialCombobox.grid(row=19, column=0, sticky=W, pady=2)
+
+    hobbyLb = Label(ActionWindow, text="Hobby")
     hobbyLb.grid(row=21, column=0, sticky=W, pady=2)
+
+    n7 = StringVar()
+    hobbyCombobox = ttk.Combobox(ActionWindow, textvariable=n7)
+    hobbyCombobox['values'] = ('a', 'b')
+    hobbyCombobox.grid(row=22, column=0, sticky=W, pady=2)
